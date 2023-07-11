af09ac74f3cf1f70659b3ecb12d0693c766e7344
Srikar Reddy Gundam <srikarreddygundam@Srikars-MacBook-Air.local>
2023-07-11 16:28:40 -0500

    Commiting the code

---
+++ src/main/java/com/java/springboot/demo/repository/SubmissionRepoClass.java
@@ -0,0 +1,49 @@
+package com.java.springboot.demo.repository;
+
+import com.java.springboot.demo.entity.SubmissionDTO;
+import org.springframework.stereotype.Repository;
+
+import java.util.*;
+
+@Repository
+public class SubmissionRepoClass implements SubmissionRepository{
+    private Map<String, SubmissionDTO> submissions = new HashMap<>();
+
+    @Override
+    public SubmissionDTO getSubmission(String id) {
+
+        return submissions.get(id);
+    }
+
+    @Override
+    public List<SubmissionDTO> getAll() {
+        return new ArrayList<>(submissions.values());
+    }
+
+    @Override
+    public SubmissionDTO addSubmission(SubmissionDTO submission) {
+        String id = UUID.randomUUID().toString();
+        submission.setId(id);
+
+        submissions.put(id, submission);
+        return submission;
+    }
+
+    @Override
+    public SubmissionDTO updateSubmission(SubmissionDTO submission) {
+        SubmissionDTO sdto = new SubmissionDTO();
+        sdto.setId(submission.getId());
+        sdto.setSubmissionDate(submission.getSubmissionDate());
+        sdto.setRate(submission.getRate());
+        sdto.setTechnology(submission.getTechnology());
+        sdto.setVendorName(submission.getVendorName());
+        sdto.setConsultantName(submission.getConsultantName());
+        return sdto;
+    }
+
+    @Override
+    public SubmissionDTO deleteSubmission(String id) {
+        return submissions.remove(id);
+    }
+
+}
